7683fcc8384956a920f912a463e044339107b644
Shelli Silverstein <robot.shelli@gmail.com>
2024-10-07 11:29:36 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -47,10 +47,35 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
+class Login(Resource):
+    def post(self):
+        user = User.query.filter(User.username == request.get_json()['username']).first()
+        session['user_id'] = user.id
+        return user.to_dict()
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if not user_id:
+            return {}, 401
+        user = User.query.get(user_id)
+        if user:
+            return user.to_dict(), 200
+
+        return {'message': 'User not found'}, 404
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 
 if __name__ == '__main__':
